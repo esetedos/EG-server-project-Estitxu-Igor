2548a32fd3663aba1ce9f6d5a66262417f471731
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-12-11 18:18:34 +0100

    EG-176 [DELETE] deleted cron and changes in socketMain

--- services/sockets/socketMain.js
+++ services/sockets/socketMain.js
@@ -2,10 +2,8 @@ const server = require('../../index')
 const io = server.socketIO;
 
 const socketEvents = require('./socketEvents').socketEvents;
-const cron = require('../cron/cronEvent.js').myCronJob;
 
 io.on('connection', socketEvents);
-io.on('connection', cron)
 
 
 
