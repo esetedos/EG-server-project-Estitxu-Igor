538127739f30ae6e7c8824ea54f6b91fb0224d79
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-12-11 18:14:15 +0100

    EG-176 [UPDATE] cron function at index

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const cron = require('node-cron');
 const schedule = require('node-schedule');
 
-const myCronJob = require('./services/cron/cronEvent')
+// const myCronJob = require('./services/cron/cronEvent')
 
 admin.initializeApp({
     credential: admin.credential.cert({
@@ -78,6 +78,37 @@ app.get('/', (req, res) => {
     res.send('Welcome to Absentia Socket Service')
 })
 
+const myCronJob = async () => {
+    const penaltyStamina = -10;
+    const penaltyAg = -5;
+    const penaltyStr = -2;
+    const userList = await userService.getAllUsers();
+    const newUserList = [];
 
+    for(const user of userList){
+      if(user.rol == "Acolito"){ 
+
+        const newStats = {
+            stamina: user.characterStats.stamina + penaltyStamina < 1?0:user.characterStats.stamina + penaltyStamina,
+            agility: user.characterStats.agility + penaltyAg <1?0:user.characterStats.agility + penaltyAg,
+            strength: user.characterStats.strength + penaltyStr <1?0:user.characterStats.strength + penaltyStr,
+            intelligence: user.characterStats.intelligence
+
+        }
+
+        const newUser = await userService.updatedUser(user.email, "characterStats", newStats)
+
+        console.log()
+
+        newUserList.push(newUser[0]);
+
+      }
+      else{
+        newUserList.push(user);
+      }
+    }
+    console.log('*************** CRON TRIGGERED ***********')
+    io.emit("userList", newUserList);
+  };
 
 require('./services/sockets/socketMain');
