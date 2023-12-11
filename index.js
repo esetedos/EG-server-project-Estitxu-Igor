b78b792c2bd3d10bce70ef5abc188b721983be9b
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-12-11 18:31:22 +0100

    EG-176 [UPDATE] cron forcefully triggered

--- index.js
+++ index.js
@@ -50,7 +50,38 @@ async function start(){
         });
         console.log('Conexión con Mongo correcta')
 
-        cron.schedule('*/30 * * * * *', myCronJob); //cada 2min
+        cron.schedule('*/30 * * * * *', async () => {
+            const penaltyStamina = -10;
+            const penaltyAg = -5;
+            const penaltyStr = -2;
+            const userList = await userService.getAllUsers();
+            const newUserList = [];
+        
+            for(const user of userList){
+              if(user.rol == "Acolito"){ 
+        
+                const newStats = {
+                    stamina: user.characterStats.stamina + penaltyStamina < 1?0:user.characterStats.stamina + penaltyStamina,
+                    agility: user.characterStats.agility + penaltyAg <1?0:user.characterStats.agility + penaltyAg,
+                    strength: user.characterStats.strength + penaltyStr <1?0:user.characterStats.strength + penaltyStr,
+                    intelligence: user.characterStats.intelligence
+        
+                }
+        
+                const newUser = await userService.updatedUser(user.email, "characterStats", newStats)
+        
+                console.log()
+        
+                newUserList.push(newUser[0]);
+        
+              }
+              else{
+                newUserList.push(user);
+              }
+            }
+            console.log('*************** CRON TRIGGERED ***********')
+            io.emit("userList", newUserList);
+          }); //cada 2min
         cron.schedule('*/30 * * * * *', () => {
             console.log('Second cron triggered')
         });
