9268e7841bbf9d95a50974d219c9bc3c6880d149
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-10 08:50:15 +0100

    EG-182 Keychain function commited until keychain secret adquisition

--- src/keychain/keychain.js
+++ src/keychain/keychain.js
@@ -1,22 +1,22 @@
-import * as Keychain from 'react-native-keychain';
-import {KEYCHAIN_SECRET} from '@env'
+// import * as Keychain from 'react-native-keychain';
+// import {KEYCHAIN_SECRET} from '@env'
 
-export const setSecureValue = async (key, value) => {
-    console.log("SETTING SECURE VALUE")
-    await Keychain.setInternetCredentials(key, KEYCHAIN_SECRET, value)   
- }
+// export const setSecureValue = async (key, value) => {
+//     console.log("SETTING SECURE VALUE")
+//     await Keychain.setInternetCredentials(key, KEYCHAIN_SECRET, value)   
+//  }
  
-export const getSecureValue = async (key) => {
-    console.log("GETTING SECURE VALUE")
-    const result = await Keychain.getInternetCredentials(key)
-    if (result) {
-        return result.password  
-    }
-return false
-}
+// export const getSecureValue = async (key) => {
+//     console.log("GETTING SECURE VALUE")
+//     const result = await Keychain.getInternetCredentials(key)
+//     if (result) {
+//         return result.password  
+//     }
+// return false
+// }
 
-export const removeSecureValue = async (key) => {
-    console.log("REMOVING SECURE VALUE")
-    await Keychain.resetInternetCredentials(key)
-}
+// export const removeSecureValue = async (key) => {
+//     console.log("REMOVING SECURE VALUE")
+//     await Keychain.resetInternetCredentials(key)
+// }
 
