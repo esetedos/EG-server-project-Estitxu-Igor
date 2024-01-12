33a3827307215d7108b628cd534045bb635b4752
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-01-12 13:31:02 +0100

    EG-187 [UPDATE] removed more time for token expiration

--- src/JWT/generateJWT.js
+++ src/JWT/generateJWT.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const generateAccessToken = (email) => {
-   const result = jwt.sign({data: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 40000 }) //1 day in miliseconds (until refresh is done)
+   const result = jwt.sign({data: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 100 }) //1 day in miliseconds (until refresh is done)
    console.log('***************** ACCESS TOKEN **************')
    console.log(result)
    return result
