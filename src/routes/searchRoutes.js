cc0ee95e9caf4d6e0d7629bfc9a649bc5fa69f3e
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-12 12:31:50 +0100

    EG-187 [UPDATE] middleware implementation to all initial get routes

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const searchController = require('../controllers/searchControllers')
 
-router.get('/', searchController.getSearch)
+const  middleware = require("../middlewares/middleware")
+
+router.get('/', middleware.authenticateToken,searchController.getSearch)
 router.patch("/", searchController.updateState)
 
 module.exports = router;
